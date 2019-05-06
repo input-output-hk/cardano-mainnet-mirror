cf970d9c2b2484746c99dea86309143cf047bce8
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-05-07 08:10:07 +1000

    Improve ability of code to find the epoch data
    The epoch data can now be found by:
    
    * Setting the "CARDANO_MAINNET_MIRROR" variable to point to it.
    * Using 'getDataDir' provided by the build system (which does not work with
      "cabal new-build").
    
    It also fails hard if the data cannot be found.

--- src/Cardano/Mirror.hs
+++ src/Cardano/Mirror.hs
@@ -4,15 +4,28 @@ module Cardano.Mirror
 where
 
 import Data.List (sort)
-import Paths_cardano_mainnet_mirror
-import System.Directory
-import System.FilePath
+import Paths_cardano_mainnet_mirror (getDataDir)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.Environment (lookupEnv)
+import System.Exit (exitFailure)
+import System.FilePath ((</>), isExtensionOf)
 
+-- Failing here (with 'exitFailure') is fine because this function is only ever
+-- used to test maiinnet validaton. It is never used in production code.
 
 mainnetEpochFiles :: IO [FilePath]
-mainnetEpochFiles = do
-  dataDir <- getDataDir
-  sort
-    .   fmap (dataDir </>)
-    .   filter ("epoch" `isExtensionOf`)
-    <$> getDirectoryContents dataDir
+mainnetEpochFiles =
+  -- Lookup the environment variable first and if that fails, use the one
+  -- provided by cabal (using 'Paths_cardano_mainnet_mirror').
+  lookupEnv "CARDANO_MAINNET_MIRROR"
+    >>= maybe getDataDir pure
+    >>= \ fpath -> do
+      exists <- doesDirectoryExist fpath
+      if exists
+        then sort
+              . fmap (fpath </>)
+              . filter ("epoch" `isExtensionOf`)
+              <$> getDirectoryContents fpath
+        else do
+          putStrLn $ "mainnetEpochFiles: directory '" ++ fpath ++ "' does not exist."
+          exitFailure
